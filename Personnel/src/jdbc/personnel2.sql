5c2cc5b6dfa7bf0f40ba43ee4f218c36f4be8bd3
Anders Emmanuel <andersemmanuel93@gmail.com>
2026-02-06 15:08:15 +0100

    scripte création de tables 3
    scripte création de tables 3

--- Personnel/src/jdbc/personnel2.sql
+++ Personnel/src/jdbc/personnel2.sql
@@ -3,7 +3,6 @@ CREATE TABLE Employe (
     nom_Employe VARCHAR(50) NOT NULL,
     prenom_Employe VARCHAR(50) NOT NULL,
     password_Employe VARCHAR(255) NOT NULL,
-    poste VARCHAR(50),
     date_arrivee DATE,
     date_depart DATE,
     numero_Ligue INT NULL
@@ -20,12 +19,10 @@ ALTER TABLE Employe
 ADD CONSTRAINT fk_employe_ligue
 FOREIGN KEY (numero_Ligue)
 REFERENCES Ligue(numero_Ligue)
-ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 ALTER TABLE Ligue
 ADD CONSTRAINT fk_ligue_admin
 FOREIGN KEY (numero_Admin)
 REFERENCES Employe(numero_Employe)
-ON DELETE RESTRICT
 ON UPDATE CASCADE;
