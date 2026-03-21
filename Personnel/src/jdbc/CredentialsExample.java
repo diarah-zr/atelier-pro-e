3d98a055d2c529646567753af7eefa8721b22755
diarah-zr <diarahtraore78@gmail.com>
2026-03-21 13:18:12 +0100

    mcd
    deuxième mcd effacer

--- Personnel/src/jdbc/CredentialsExample.java
+++ Personnel/src/jdbc/CredentialsExample.java
@@ -4,11 +4,11 @@ public class CredentialsExample
 {
 	private static String driver = "mysql";
 	private static String driverClassName = "com.mysql.cj.jdbc.Driver";
-	private static String host = "localhost";
+	private static String host = "192.168.112.147";
 	private static String port = "3306";
-	private static String database = "";
-	private static String user = "";
-	private static String password = "";
+	private static String database = "personnel";
+	private static String user = "personnel";
+	private static String password = "personnel";
 	
 	static String getUrl() 
 	{
